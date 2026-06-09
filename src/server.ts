94a26aca8266e94a2aa414b21636af34f13a2848
Connor Callahan <connor@livefront.com>
2026-06-09 15:45:40 -0500

    Create initial Swagger doc; host @ /docs

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
 
 const app = express()
+
+// Host the API Swagger doc on the server
+const swaggerDocument = YAML.load('./spec/api.yaml')
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(express.json())
 
