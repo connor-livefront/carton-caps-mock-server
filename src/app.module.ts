76a15f18ba9ee776056e195086b72c5bd33b1daa
Connor Callahan <connor@livefront.com>
2026-06-16 13:55:29 -0500

    Add Nest Swagger doc support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { ReferralsModule } from './referrals/referrals.module';
 
 @Module({
   imports: [ReferralsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Carton Caps Referral API')
+    .setDescription('REST API for the refer-a-friend feature')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
